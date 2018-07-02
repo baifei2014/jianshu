c4344de63aa7366e986d959f4cf183fcf6b560be
蒋龙豪 <759395919@qq.com>
2018-07-02 13:08:14 +0800

    nothing

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -40,6 +40,7 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '/chat' => 'chat/default/index',
             ],
         ],
     ],
